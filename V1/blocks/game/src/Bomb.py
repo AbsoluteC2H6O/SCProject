468ea7850d9e100ef3fec979d4c99bd799d13fec
Abraham Mousalli <locutor.abraham@gmail.com>
2023-05-13 18:12:16 -0400

    SCProject se incorpora el efecto de explosión de la bomba cuando el agente esta encima de la misma

--- V1/blocks/game/src/Bomb.py
+++ V1/blocks/game/src/Bomb.py
@@ -3,12 +3,15 @@ from .. import settings
 from .Entity import Entity
 from .Tilemap import TileMap
 
-
 class Bomb(Entity):
     def __init__(self, x, y, scene):
+        self.bombx = x
+        self.bomby = y
+        self.sc = scene
         super().__init__(x, y, settings.TILE_SIZE, settings.TILE_SIZE, "bomb", 0, scene)
 
     def explosion(self):
         i, j = TileMap.to_map(self.x, self.y)
 
         self.tile_map.tiles[i][j].busy = True
+        super().__init__(self.bombx, self.bomby, settings.TILE_SIZE, settings.TILE_SIZE, "explosion", 0, self.sc)
